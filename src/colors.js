285774c8328c8fdfdc9f0fcd4ac9344157312d9d
levi-zustiak <levizustiak@gmail.com>
2022-11-26 19:28:12 -0600

    [feat] add color file

---
+++ src/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  blue: "#52a9ff",
+  red: "#ff5c61",
+  purple: "#c072f8",
+  orange: "#fcaf4a",
+  green: "#7be0ad",
+  white: "#ededee",
+  "black-100": "#141617",
+  "black-200": "#212121",
+  "black-300": "#2a2a2a",
+  "black-400": "#343434",
+  background: "#141617",
+  border: "#282b2d",
+  operator: "#81888d",
+};

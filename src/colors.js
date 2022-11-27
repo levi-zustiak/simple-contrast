18cd009f400b1b55e8761c9669f5eafe0e8a8dd4
levi-zustiak <levizustiak@gmail.com>
2022-11-27 00:00:46 -0600

    [feat] added more theme colors to editor

--- src/colors.js
+++ src/colors.js
@@ -3,7 +3,10 @@ const colors = {
   red: "#ff5c61",
   purple: "#c072f8",
   orange: "#fcaf4a",
-  green: "#7be0ad",
+  green: "#1eae5a",
+  yellow: "#FFF275",
+  yellow: "#fee440",
+  turquise: "#7be0ad",
   white: "#ededee",
   "black-100": "#141617",
   "black-200": "#212121",
@@ -11,5 +14,6 @@ const colors = {
   "black-400": "#343434",
   background: "#141617",
   border: "#282b2d",
+  contrastBorder: "#9b9b9b",
   operator: "#81888d",
 };
